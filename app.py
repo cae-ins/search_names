f48b6bddeba58bd0d6fedf85366d4eab8e5849d0
datalab <datalab@Jarvis.local>
2025-04-24 19:43:40 +0000

    Ajout de la colonne annee dans la base ES et reajustement de l API

--- app.py
+++ app.py
@@ -3,55 +3,31 @@ from elasticsearch import Elasticsearch
 
 app = FastAPI()
 
-# Connexion à Elasticsearch dans Kubernetes
 es = Elasticsearch("http://elasticsearch.elasticsearch.svc.cluster.local:9200")
 
 @app.get("/recherche")
 def recherche_nom(nom: str = Query(..., min_length=2)):
-    query = {
-        "query": {
-            "term": {
-                "nom.keyword": nom 
-            }
-        }
-    }
-
-    result = es.search(index="noms_prenoms", body=query, size=100)
-    hits = [hit["_source"] for hit in result["hits"]["hits"]]
-
-    if not hits:
-        return {
-            "message": f"Aucun document trouvé pour le nom : '{nom}'"
-        }
-
-    return {
-        "resultats": hits
-    }
-
-'''
-from fastapi import FastAPI, Query
-from elasticsearch import Elasticsearch
-
-app = FastAPI()
-
-#es = Elasticsearch("http://192.168.1.230:30920")
-es = Elasticsearch("http://elasticsearch.elasticsearch.svc.cluster.local:9200")
-
-
-@app.get("/recherche")
-def recherche_nom(nom: str = Query(..., min_length=2)):
-    query = {
-        "query": {
-            "match": {
-                "nom": {
-                    "query": nom,
-                    "fuzziness": 2
+    try:
+        # On fait une recherche exacte sur le nom
+        query = {
+            "query": {
+                "term": {
+                    "nom.keyword": nom  
                 }
             }
         }
-    }
 
-    result = es.search(index="noms_prenoms", body=query)
-    hits = [hit["_source"] for hit in result["hits"]["hits"]]
-    return {"resultats": hits}
-'''
+        result = es.search(index="noms_prenoms", query=query["query"], size=100)
+        hits = [hit["_source"] for hit in result["hits"]["hits"]]
+
+        if not hits:
+            return {
+                "message": f"Aucun document trouvé pour le nom exact : '{nom}'"
+            }
+
+        return {
+            "resultats": hits
+        }
+
+    except Exception as e:
+        return {"error": str(e)}
